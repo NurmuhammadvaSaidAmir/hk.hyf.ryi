1fc14e6b8bb5c3f7db0901d0a9a73cedaf7ac561
NurmuhammadvaSaidAmir <clash010a@gmail.com>
2025-03-26 22:40:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let name1 = prompt('Ismingizni kiriting');
+let id = prompt('idine kiriting');
+let pasword = prompt ('Paswordi kiriting');
+
+console.log(`name ${name1}`);
+console.log(`id ${id}`);
+console.log(`pasword ${pasword}`);
